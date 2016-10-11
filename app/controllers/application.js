9f5e27c4873e65adb64ff90a9198510093a9a6ea
Matt Payne <Payne@MattPayne.org>
2016-10-11 17:36:24 -0500

    exercise: who is here

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,5 +3,8 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   name: `Matt "Nice Guy" Payne`,
   chefName: `chef`,
-  chefs: ['Gordon Ramsay','Anthony Bourdain']
+  chefs: [
+    {name: 'Gordon Ramsay', cookingToday: true},
+    {name: 'Anthony Bourdain', cookingToday: false},
+  ]
 });

24f2f0998b02b9ace186fbdc8573e974cfbbe23b
Matt Payne <Payne@MattPayne.org>
2016-10-11 18:24:44 -0500

    exercise: relocatin the chefs

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,10 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   name: `Matt "Nice Guy" Payne`,
   chefName: `chef`,
-  chefs: [
-    {name: 'Gordon Ramsay', cookingToday: true},
-    {name: 'Anthony Bourdain', cookingToday: false},
-  ],
   actions: {
     toggleCookingToday(chef) {
       Ember.set(chef, 'cookingToday', !chef.cookingToday);
--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return [
+        {name: 'Gordon Ramsay', cookingToday: true},
+        {name: 'Anthony Bourdain', cookingToday: false},
+      ];
+  }
 });

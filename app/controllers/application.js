f11f026b9d9c74d16ee9baca305df6c16d95b927
Matt Payne <Payne@MattPayne.org>
2016-10-14 18:17:04 -0500

    New chef records can be created.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,12 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   name: `Matt "Nice Guy" Payne`,
   chefName: `chef`,
+  newChefName: '',
   actions: {
     toggleCookingToday(chef) {
       console.log("About to toggle: " + chef.get('name'));
       Ember.set(chef, 'cookingToday', !chef.get('cookingToday'));
       chef.save();
       console.log("\tNew value is " + chef.get('cookingToday'));
+    },
+    saveNewChef() {
+      console.log('saveNewChef: newChefName=' + this.newChefName);
+        this.store.createRecord('chef',{name: this.newChefName, cookingToday: false}).save();
+        this.set('newChefName','');
     }
   }
 });

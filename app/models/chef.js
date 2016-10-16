0994c68e309efcd9d25991173319e5b664e9eaf0
Matt Payne <Payne@MattPayne.org>
2016-10-16 07:42:27 -0500

    default value

--- app/models/chef.js
+++ app/models/chef.js
@@ -2,6 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  numberOfStudents: DS.attr('number'),
+  numberOfStudents: DS.attr('number', {defaultValue:0}),
   cookingToday: DS.attr('boolean')
 });

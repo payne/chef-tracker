6a611a9320f0080df14b075f8ec2aee9281c5d07
Matt Payne <Payne@MattPayne.org>
2016-10-18 17:53:09 -0500

    starting with ids

--- app/router.js
+++ app/router.js
@@ -8,6 +8,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('chefs');
+  this.route('chef', {path: 'chef/:chef_id'});
   this.route('about');
   this.route('training');
 });

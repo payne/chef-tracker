18a693839f58fe665dc9d272bd150a64b9973989
Matt Payne <Payne@MattPayne.org>
2016-10-24 20:22:04 -0500

    cleanup application route

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,17 +3,7 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    // goal, change the old records to have numberOfStudents be 0 not null.
     let allChefs = this.store.findAll('chef');
-    // this hints there is an isUpdating property, but there is not
-    // http://stackoverflow.com/questions/18138383/iterating-over-ember-js-ember-data-record-arrays
-    // outputs allChefs.isUpdating=undefined
-    console.log("allChefs.isUpdating="+allChefs.isUpdating);
-    allChefs.forEach(function(chef) {
-      console.log(chef.name +" has " + chef.numberOfStudents + " students");
-      chef.set('numberOfStudents',chef.get('numberOfStudents')+0);
-      chef.save();
-    });
     return allChefs;
   }
 });
